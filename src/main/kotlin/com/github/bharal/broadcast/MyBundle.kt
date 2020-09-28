64fcda19e850741feadfa190d3ae1723c4445aa1
GitHub Action <action@github.com>
2020-09-28 13:36:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/bharal/broadcast/MyBundle.kt
+++ src/main/kotlin/com/github/bharal/broadcast/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bharal.broadcast
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/bharal/broadcast/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bharal/broadcast/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bharal.broadcast.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bharal.broadcast.MyBundle
 
 class MyApplicationService {
 
